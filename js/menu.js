c054d85cfd52d89943edb2ddee08d08fcad82344
L17111868 <92753688+L17111868@users.noreply.github.com>
2021-10-19 11:35:24 -0600

    Delete js directory

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-let btnmenu = document.getElementById('btnmenu'); /*buscar un elemento por id*/
-let menu = document.getElementById('menu');
-btnmenu.addEventListener('click',function(){
-    'use strict';
-    menu.classList.toggle('mostrar');
-})/*al darle click, le damos una funcion de mostrar*/
